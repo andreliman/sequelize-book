c023f7ccbb75802992e9830337267a27433ca9d2
muriloignaccio <murilohsignacio@gmail.com>
2021-08-31 21:01:37 -0300

    Criar rota para adicionar endereço ao usuário

--- models/users.js
+++ models/users.js
@@ -1,6 +1,6 @@
 const { User } = require('../database/models');
 
-exports.getAllUsers = () => User.findAll({ include: 'books' });
+exports.getAllUsers = () => User.findAll({ include: ['books', 'addresses'] });
 
 exports.addBookToUser = async (id, book) => {
   const user = await User.findByPk(id);
@@ -8,4 +8,12 @@ exports.addBookToUser = async (id, book) => {
   const returnedBook = user.createBook(book);
 
   return returnedBook;
+}
+
+exports.addAddressToUser = async (id, address) => {
+  const user = await User.findByPk(id);
+
+  const returnedAddress = user.createAddress(address);
+
+  return returnedAddress;
 }
